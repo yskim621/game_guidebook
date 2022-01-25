4eeb74fa96384f35e4742d9f42fa52cc62d85143
yskim621 <yskim62100@gmail.com>
2022-01-25 11:46:02 +0900

    making project

---
+++ src/main/java/game/guidebook/GuidebookApplication.java
@@ -0,0 +1,13 @@
+package game.guidebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuidebookApplication.class, args);
+	}
+
+}
