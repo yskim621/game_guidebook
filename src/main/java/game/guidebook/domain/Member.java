692d698f296f263dfed5ce6046a553b45b7f207f
yskim621 <yskim62100@gmail.com>
2022-02-01 14:07:41 +0900

    Making Member entity

--- src/main/java/game/guidebook/domain/Member.java
+++ src/main/java/game/guidebook/domain/Member.java
@@ -1,4 +1,21 @@
 package game.guidebook.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
