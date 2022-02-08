ba6f457817023bab9add025a19bf7f43f6071039
yskim621 <yskim62100@gmail.com>
2022-02-08 16:00:33 +0900

    Updating Member Entity

--- src/main/java/game/guidebook/domain/Member.java
+++ src/main/java/game/guidebook/domain/Member.java
@@ -11,11 +11,81 @@ public class Member {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    private String memberemail;
+
+    private String memberpassword;
+
+    private String membername;
+
+    private String memberphone;
+
+    private String membernickname;
+
+    private String membergender;
+
+    private String memberleave;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getMemberemail() {
+        return memberemail;
+    }
+
+    public void setMemberemail(String memberemail) {
+        this.memberemail = memberemail;
+    }
+
+    public String getMemberpassword() {
+        return memberpassword;
+    }
+
+    public void setMemberpassword(String memberpassword) {
+        this.memberpassword = memberpassword;
+    }
+
+    public String getMembername() {
+        return membername;
+    }
+
+    public void setMembername(String membername) {
+        this.membername = membername;
+    }
+
+    public String getMemberphone() {
+        return memberphone;
+    }
+
+    public void setMemberphone(String memberphone) {
+        this.memberphone = memberphone;
+    }
+
+    public String getMembernickname() {
+        return membernickname;
+    }
+
+    public void setMembernickname(String membernickname) {
+        this.membernickname = membernickname;
+    }
+
+    public String getMembergender() {
+        return membergender;
+    }
+
+    public void setMembergender(String membergender) {
+        this.membergender = membergender;
+    }
+
+    public String getMemberleave() {
+        return memberleave;
+    }
+
+    public void setMemberleave(String memberleave) {
+        this.memberleave = memberleave;
     }
 }
