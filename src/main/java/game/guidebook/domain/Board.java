f5d94b780872638badba1baac4be821a4fa3ebce
yskim621 <yskim62100@gmail.com>
2022-02-11 16:05:48 +0900

    Updating board Entity

--- src/main/java/game/guidebook/domain/Board.java
+++ src/main/java/game/guidebook/domain/Board.java
@@ -2,14 +2,27 @@ package game.guidebook.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Board {
 
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue
     private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String writeDate;
+
+    private Integer readCnt;
+
+    private String attachment;
+
+    private String memberNickname;
 
     public Long getId() {
         return id;
@@ -17,5 +30,53 @@ public class Board {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWriteDate() {
+        return writeDate;
+    }
+
+    public void setWriteDate(String writeDate) {
+        this.writeDate = writeDate;
+    }
+
+    public Integer getReadCnt() {
+        return readCnt;
+    }
+
+    public void setReadCnt(Integer readCnt) {
+        this.readCnt = readCnt;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+    public String getMemberNickname() {
+        return memberNickname;
+    }
+
+    public void setMemberNickname(String memberNickname) {
+        this.memberNickname = memberNickname;
     }
 }
