ec447b2de8716014d365f2b3a35e4ea47536ba43
yskim621 <yskim62100@gmail.com>
2022-02-14 10:25:17 +0900

    Updating Game Entity

--- src/main/java/game/guidebook/domain/Game.java
+++ src/main/java/game/guidebook/domain/Game.java
@@ -1,4 +1,69 @@
 package game.guidebook.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * Game
+ * Trump Card , Board Game , Korean Card Game Info Entity
+ * @author 김윤석
+ * @CreateDate 2/8/22
+ * @UpdateDate
+**/
+@Entity
 public class Game {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String gameCode;
+
+    private String gameCategory;
+
+    private String gameInfo;
+
+    private String gameRule;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGameCode() {
+        return gameCode;
+    }
+
+    public void setGameCode(String gameCode) {
+        this.gameCode = gameCode;
+    }
+
+    public String getGameCategory() {
+        return gameCategory;
+    }
+
+    public void setGameCategory(String gameCategory) {
+        this.gameCategory = gameCategory;
+    }
+
+    public String getGameInfo() {
+        return gameInfo;
+    }
+
+    public void setGameInfo(String gameInfo) {
+        this.gameInfo = gameInfo;
+    }
+
+    public String getGameRule() {
+        return gameRule;
+    }
+
+    public void setGameRule(String gameRule) {
+        this.gameRule = gameRule;
+    }
 }
