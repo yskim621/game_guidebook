91513c4e7cc83f3914864b0ea9e74ac574a74152
yskim621 <yskim62100@gmail.com>
2022-03-02 10:04:42 +0900

    Updating BoardRepository

---
+++ src/main/java/game/guidebook/repository/BoardRepository.java
@@ -0,0 +1,19 @@
+package game.guidebook.repository;
+
+import game.guidebook.domain.Board;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardRepository {
+
+    private final EntityManager em;
+
+    public List<Board> findAll(){
+        return em.createQuery("select b from Board b", Board.class).getResultList();
+    }
+}
