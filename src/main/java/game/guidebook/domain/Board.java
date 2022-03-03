9d2b82d2149174236346955328a66578f72abeb7
yskim621 <yskim62100@gmail.com>
2022-03-04 00:17:03 +0900

    Updating Board Entity

--- src/main/java/game/guidebook/domain/Board.java
+++ src/main/java/game/guidebook/domain/Board.java
@@ -1,11 +1,15 @@
 package game.guidebook.domain;
 
-import javax.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Board {
 
     @Id
@@ -23,60 +27,4 @@ public class Board {
     private String attachment;
 
     private String memberNickname;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getWriteDate() {
-        return writeDate;
-    }
-
-    public void setWriteDate(String writeDate) {
-        this.writeDate = writeDate;
-    }
-
-    public Integer getReadCnt() {
-        return readCnt;
-    }
-
-    public void setReadCnt(Integer readCnt) {
-        this.readCnt = readCnt;
-    }
-
-    public String getAttachment() {
-        return attachment;
-    }
-
-    public void setAttachment(String attachment) {
-        this.attachment = attachment;
-    }
-
-    public String getMemberNickname() {
-        return memberNickname;
-    }
-
-    public void setMemberNickname(String memberNickname) {
-        this.memberNickname = memberNickname;
-    }
 }
