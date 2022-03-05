eedcca80eb93b2278dbe394afbaefc7912921496
yskim621 <yskim62100@gmail.com>
2022-03-05 17:07:46 +0900

    Updating BoardApiController

--- src/main/java/game/guidebook/api/BoardApiController.java
+++ src/main/java/game/guidebook/api/BoardApiController.java
@@ -1,15 +1,17 @@
 package game.guidebook.api;
 
+import game.common.persist.query.SearchResult;
+import game.guidebook.api.dto.BoardDto;
 import game.guidebook.domain.Board;
+import game.guidebook.repository.BoardRepository;
 import game.guidebook.service.BoardService;
+import game.guidebook.service.dto.QueryParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,10 +19,21 @@ import java.util.List;
 public class BoardApiController {
 
     private final BoardService boardService;
+    private final BoardRepository boardRepository;
 
     @PostMapping("ajaxList")
-    public List<Board> list(Model model) {
-        return boardService.list();
+    public SearchResult<BoardDto> list(QueryParam query_param, Model model,
+                               @RequestParam(value = "offset", defaultValue = "0") int offset,
+                               @RequestParam(value = "limit", defaultValue = "100") int limit
+    ) {
+        List<Board> boards = boardRepository.findAll(query_param, offset, limit);
+        List<BoardDto> collect = boards.stream()
+                .map(board -> new BoardDto(board))
+                .collect(Collectors.toList());
+
+        SearchResult<BoardDto> result = new SearchResult<>();
+        result.setResult(collect);
+        return result;
     }
 
 }
