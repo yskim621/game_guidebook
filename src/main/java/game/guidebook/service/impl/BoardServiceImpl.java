b150651afeb07f1d1f10f8cc2a6a5066535c5c55
yskim621 <yskim62100@gmail.com>
2022-03-08 16:10:55 +0900

    add BoardServiceImpl

--- src/main/java/game/guidebook/service/impl/BoardServiceImpl.java
+++ src/main/java/game/guidebook/service/impl/BoardServiceImpl.java
@@ -1,4 +1,46 @@
 package game.guidebook.service.impl;
 
-public class BoardServiceImpl {
+import game.guidebook.domain.Board;
+import game.guidebook.repository.BoardRepository;
+import game.guidebook.service.BoardService;
+import game.guidebook.service.dto.QueryParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Override
+    public List<Board> list(QueryParam query_param, int offset, int limit) {
+        return boardRepository.findAll(query_param, offset, limit);
+    }
+
+    @Override
+    public Board detail(Long id) {
+        return boardRepository.findOne(id);
+    }
+
+    @Override
+    public void create(MultipartHttpServletRequest request) {
+
+    }
+
+    @Override
+    public void update(MultipartHttpServletRequest request) {
+
+    }
+
+    @Override
+    public void delete(HttpServletRequest request) {
+
+    }
 }
