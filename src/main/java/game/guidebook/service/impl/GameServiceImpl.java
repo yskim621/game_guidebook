efb087b0ce97c99e72c80c13f76e72d7b3586013
yskim621 <yskim62100@gmail.com>
2022-03-11 00:02:06 +0900

    Updating GameServiceImpl

---
+++ src/main/java/game/guidebook/service/impl/GameServiceImpl.java
@@ -0,0 +1,9 @@
+package game.guidebook.service.impl;
+
+
+import game.guidebook.service.GameService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameServiceImpl implements GameService {
+}
