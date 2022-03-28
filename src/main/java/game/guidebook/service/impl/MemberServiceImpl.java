129edfb76452f2a7952f15ac84640c28bdbdf142
yskim621 <yskim62100@gmail.com>
2022-03-28 11:12:51 +0900

    Updating MemberService

---
+++ src/main/java/game/guidebook/service/impl/MemberServiceImpl.java
@@ -0,0 +1,9 @@
+package game.guidebook.service.impl;
+
+import game.guidebook.service.MemberService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+}
