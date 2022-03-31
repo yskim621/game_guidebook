d615480fcc24c040e0dc2d87c973b28874e11b36
yskim621 <yskim62100@gmail.com>
2022-04-01 00:22:39 +0900

    Updating BoardRepository

--- src/main/java/game/guidebook/repository/BoardRepository.java
+++ src/main/java/game/guidebook/repository/BoardRepository.java
@@ -4,6 +4,7 @@ import game.guidebook.domain.Board;
 import game.guidebook.service.dto.QueryParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -16,13 +17,22 @@ public class BoardRepository {
 
     public List<Board> findAll(QueryParam query_param, int offset, int limit){
         query_param.setPageNumber(query_param.getPageNumber());
+        query_param.setPageSize(query_param.getPageSize());
         return em.createQuery("select b from Board b", Board.class)
-                .setFirstResult(offset)
-                .setMaxResults(limit)
+                .setFirstResult(query_param.getPageNumber())
+                .setMaxResults(query_param.getPageSize())
                 .getResultList();
     }
 
     public Board findOne(Long id) {
         return em.find(Board.class, id);
     }
+
+    @Transactional
+    public Long delete(Long id) {
+        Board board = findOne(id);
+        em.remove(board);
+        return board.getId();
+    }
+
 }
