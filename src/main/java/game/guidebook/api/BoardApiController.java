6e45cafab3ff00897ceea482cc61594077268df5
yskim621 <yskim62100@gmail.com>
2022-04-02 00:06:12 +0900

    Updating BoardApiController

--- src/main/java/game/guidebook/api/BoardApiController.java
+++ src/main/java/game/guidebook/api/BoardApiController.java
@@ -1,7 +1,10 @@
 package game.guidebook.api;
 
 import game.common.persist.query.SearchResult;
+import game.common.util.Validate;
 import game.guidebook.api.dto.BoardDto;
+import game.guidebook.common.AjaxResponseBody;
+import game.guidebook.controller.BaseController;
 import game.guidebook.domain.Board;
 import game.guidebook.repository.BoardRepository;
 import game.guidebook.service.BoardService;
@@ -16,24 +19,43 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/board")
-public class BoardApiController {
+public class BoardApiController extends BaseController {
 
     private final BoardService boardService;
     private final BoardRepository boardRepository;
 
     @PostMapping("ajaxList")
-    public SearchResult<BoardDto> list(QueryParam query_param, Model model,
+    public SearchResult<BoardDto> contentList(@RequestBody QueryParam query_param, Model model,
                                @RequestParam(value = "offset", defaultValue = "0") int offset,
-                               @RequestParam(value = "limit", defaultValue = "100") int limit
+                               @RequestParam(value = "limit", defaultValue = "10") int limit
     ) {
         List<Board> boards = boardRepository.findAll(query_param, offset, limit);
         List<BoardDto> collect = boards.stream()
-                .map(board -> new BoardDto(board))
+                .map(BoardDto::new)
                 .collect(Collectors.toList());
 
         SearchResult<BoardDto> result = new SearchResult<>();
         result.setResult(collect);
+        result.setCount(collect.size());
         return result;
+    }
+
+    @PostMapping("delete/{id}")
+    public AjaxResponseBody deleteContent(@PathVariable Long id) {
+        if (Validate.validateObjectsNullOrEmpty(id))
+        {
+            return returnErrorBody("데이터가 검증되지 않았습니다, 페이지 새로고침 후 다시 시도해주세요!");
+        }
+        try
+        {
+            boardService.delete(id);
+            return returnSuccessBody();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return returnErrorBody(ex);
+        }
     }
 
 }
