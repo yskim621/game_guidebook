0ccc8857dca1973139a677c34ace7aa9ce820617
yskim621 <yskim62100@gmail.com>
2022-04-06 00:12:53 +0900

    Adding NetworkUtil

---
+++ src/main/java/game/common/util/NetworkUtil.java
@@ -0,0 +1,21 @@
+package game.common.util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class NetworkUtil
+{
+	public static String getHostName()
+	{
+		try
+		{
+			return InetAddress.getLocalHost().getCanonicalHostName();
+		}
+		catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
