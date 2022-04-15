4a9c984db66c9992991a0249141ab26ca3e5afbf
yskim621 <yskim62100@gmail.com>
2022-04-16 00:17:18 +0900

    Create BoardDto

---
+++ src/main/java/game/guidebook/api/dto/BoardDto.java
@@ -0,0 +1,39 @@
+package game.guidebook.api.dto;
+
+import game.guidebook.domain.Board;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Column;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class BoardDto {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Date writeDate;
+
+    private Integer readCnt;
+
+    private String attachment;
+
+    private String userNickname;
+
+//    private Long userId;
+
+    public BoardDto(Board board) {
+        id = board.getId();
+        title = board.getTitle();
+        content = board.getContent();
+        writeDate = board.getWriteDate();
+        readCnt = board.getReadCnt();
+        attachment = board.getAttachment();
+        userNickname = board.getUserNickname();
+    }
+}
