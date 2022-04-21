a33581673abcc3793b15665c699d4271be45a765
yskim621 <yskim62100@gmail.com>
2022-04-22 00:01:56 +0900

    Creating GameController

---
+++ src/main/java/game/guidebook/controller/GameController.java
@@ -0,0 +1,39 @@
+package game.guidebook.controller;
+
+import game.guidebook.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value="game")
+public class GameController extends BaseController {
+
+    private final GameService gameService;
+
+    @GetMapping("trumpcard")
+    public String trumpcardView() {
+
+        return "game/trumpcard";
+    }
+
+    @GetMapping("hwatu")
+    public String hwatuView() {
+
+        return "game/hwatu";
+    }
+
+    @GetMapping("boardgame")
+    public String boardgameView() {
+
+        return "game/boardgame";
+    }
+
+}
