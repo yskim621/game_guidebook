1c46c79f21bcfb1fa4281893d1bf355e601b21b6
yskim621 <yskim62100@gmail.com>
2022-05-08 00:44:43 +0900

    Making RegisterDto

---
+++ src/main/java/game/guidebook/controller/dto/RegisterDto.java
@@ -0,0 +1,22 @@
+package game.guidebook.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+
+@Getter
+@Setter
+public class RegisterDto implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private String mobile;
+	private String email;
+	private String password;
+	private String ip;
+	private String validCode;
+
+}
