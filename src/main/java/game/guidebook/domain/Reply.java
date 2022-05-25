4300c9c4f022b35df0cd5f02633ff492b4cbdb82
yskim621 <yskim62100@gmail.com>
2022-05-26 00:51:33 +0900

    Updating Reply.java

--- src/main/java/game/guidebook/domain/Reply.java
+++ src/main/java/game/guidebook/domain/Reply.java
@@ -1,4 +1,42 @@
 package game.guidebook.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Reply {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String message;
+
+    private int boardId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(int boardId) {
+        this.boardId = boardId;
+    }
 }
