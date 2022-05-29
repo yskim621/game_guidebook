207e6fb827dd2454a13ede7ebb27e4fcb06215dc
yskim621 <yskim62100@gmail.com>
2022-05-30 05:36:29 +0900

    updating GuidebookApplication

--- src/main/java/game/guidebook/GuidebookApplication.java
+++ src/main/java/game/guidebook/GuidebookApplication.java
@@ -2,8 +2,9 @@ package game.guidebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude  = { SecurityAutoConfiguration.class })
 public class GuidebookApplication {
 
 	public static void main(String[] args) {
