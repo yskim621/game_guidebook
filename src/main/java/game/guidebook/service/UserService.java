86103c92e4eb52328962e6704d9572fb2a651f1d
yskim621 <yskim62100@gmail.com>
2022-06-05 00:30:54 +0900

    Creating UserService

---
+++ src/main/java/game/guidebook/service/UserService.java
@@ -0,0 +1,24 @@
+package game.guidebook.service;
+
+import game.guidebook.controller.dto.RegisterDto;
+import game.guidebook.domain.User;
+import net.nurigo.java_sdk.api.Message;
+import org.json.simple.JSONObject;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public interface UserService {
+
+    public User getById(Long id);
+
+    public boolean existUserName(String userName) throws Exception;
+
+    public boolean existMobile(String mobile) throws Exception;
+
+    public boolean existEmail(String email) throws Exception;
+
+    public Long register(RegisterDto dto);
+
+    public String sendSms(String mobileNo);
+}
