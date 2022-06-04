ba9e6e54faaf1b11eb9ccd7c49cf948be85960d3
yskim621 <yskim62100@gmail.com>
2022-06-04 16:19:30 +0900

    Updating trumpcard css / js / html

---
+++ src/main/resources/static/js/game/trumpcard.js
@@ -0,0 +1,16 @@
+function pokerChangeImg(){
+	document.querySelector('#poker').style.display = "none";
+	document.querySelector('#poker_hover').style.display = "block";	
+}
+  
+function pokerShowOriginal() {	
+	document.querySelector('#poker_hover').style.display = "none";
+	document.querySelector('#poker').style.display = "block";
+}
+
+document.querySelector('#poker_hover').style.display = "none";
+document.querySelector('#blackjack_hover').style.display = "none";
+document.querySelector('#bacara_hover').style.display = "none";
+document.querySelector('#hoola_hover').style.display = "none";
+
+
