55087e59b7b6b39abd510f6a5477d8ae5373820f
yskim621 <yskim62100@gmail.com>
2022-06-06 01:05:21 +0900

    Updating UserRepository

--- src/main/java/game/guidebook/repository/UserRepository.java
+++ src/main/java/game/guidebook/repository/UserRepository.java
@@ -1,49 +1,26 @@
 package game.guidebook.repository;
 
-import game.guidebook.domain.Board;
 import game.guidebook.domain.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
+import java.util.List;
+import java.util.Optional;
 
-@Repository
-@RequiredArgsConstructor
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    private final EntityManager em;
+    @Override
+    <S extends User> S saveAndFlush(S entity);
 
-    public User findOne(Long id) {
-        return em.find(User.class, id);
-    }
+    @Override
+    User getById(Long id);
 
-    /**
-     * @return 유저 이름 중복 검사
-     */
-    public boolean existUserName(String name) throws NoResultException {
-        return em.createQuery("select u from User u where u.name= :name", User.class)
-                .setParameter("name", name)
-                .getSingleResult() != null;
-    };
+    // select u from User u where u.name =
+    List<User> findByName(String name);
 
-    /**
-     *
-     * @return 유저 휴대폰 번호 중복 검사
-     */
-    public boolean existMobile(String mobile) throws NoResultException {
-        return em.createQuery("select u from User u where u.mobile= :mobile", User.class)
-                .setParameter("mobile", mobile)
-                .getSingleResult() != null;
-    };
+    List<User> findByMobile(String mobile);
 
-    /**
-     *
-     * @return 유저 이메일 중복 검사
-     */
-    public boolean existEmail(String email) throws NoResultException {
-        return em.createQuery("select u from User u where u.email= :email", User.class)
-                .setParameter("email", email)
-                .getSingleResult() != null;
-    };
+    List<User> findByEmail(String email);
+
+
 }
---
+++ src/main/java/game/guidebook/repository/UserRepositoryOld.java
@@ -0,0 +1,60 @@
+package game.guidebook.repository;
+
+import game.guidebook.domain.Board;
+import game.guidebook.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryOld {
+
+    private final EntityManager em;
+
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    public User findOne(Long id) {
+        return em.find(User.class, id);
+    }
+
+    /**
+     * @return 유저 이름 중복 검사
+     */
+    public boolean existUserName(String name) throws Exception {
+        Optional<User> optUserByName = Optional.ofNullable(
+                em.createQuery("select u from User u where u.name= :name", User.class)
+                .setParameter("name", name)
+                .getSingleResult());
+        return optUserByName.isPresent();
+    };
+
+    /**
+     *
+     * @return 유저 휴대폰 번호 중복 검사
+     */
+    public boolean existMobile(String mobile) throws Exception {
+        Optional<User> optUserByMobile = Optional.ofNullable(
+                em.createQuery("select u from User u where u.mobile= :mobile", User.class)
+                .setParameter("mobile", mobile)
+                .getSingleResult());
+        return optUserByMobile.isPresent();
+    };
+
+    /**
+     *
+     * @return 유저 이메일 중복 검사
+     */
+    public boolean existEmail(String email) throws Exception {
+        Optional<User> optUserByEmail = Optional.ofNullable(
+                em.createQuery("select u from User u where u.email= :email", User.class)
+                .setParameter("email", email)
+                .getSingleResult());
+        return optUserByEmail.isPresent();
+    };
+}
