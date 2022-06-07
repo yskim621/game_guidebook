46d06228d0ea39070cf3370b53ae84276a5c7cd9
yskim621 <yskim62100@gmail.com>
2022-06-08 06:26:10 +0900

    Adding ReplyService

---
+++ src/main/java/game/guidebook/service/impl/ReplyServiceImpl.java
@@ -0,0 +1,9 @@
+package game.guidebook.service.impl;
+
+import game.guidebook.service.ReplyService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReplyServiceImpl implements ReplyService {
+
+}
