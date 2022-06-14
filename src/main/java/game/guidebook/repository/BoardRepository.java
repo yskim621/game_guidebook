fea0973c31f18c570ea469e09ab1ace1869caf01
yskim621 <yskim62100@gmail.com>
2022-06-15 01:10:14 +0900

    Updating BoardRepository

--- src/main/java/game/guidebook/repository/BoardRepository.java
+++ src/main/java/game/guidebook/repository/BoardRepository.java
@@ -1,38 +1,9 @@
 package game.guidebook.repository;
 
 import game.guidebook.domain.Board;
-import game.guidebook.service.dto.QueryParam;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import java.util.List;
+public interface BoardRepository extends JpaRepository<Board, Long> {
 
-@Repository
-@RequiredArgsConstructor
-public class BoardRepository {
-
-    private final EntityManager em;
-
-    public List<Board> findAll(QueryParam query_param, int offset, int limit){
-        query_param.setPageSize(query_param.getPageSize());
-        query_param.setPageNumber(query_param.getPageNumber());
-        return em.createQuery("select b from Board b", Board.class)
-                .setFirstResult(offset)
-                .setMaxResults(limit)
-                .getResultList();
-    }
-
-    public Board findOne(Long id) {
-        return em.find(Board.class, id);
-    }
-
-    @Transactional
-    public Long delete(Long id) {
-        Board board = findOne(id);
-        em.remove(board);
-        return board.getId();
-    }
 
 }
