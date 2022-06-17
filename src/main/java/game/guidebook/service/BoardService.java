bd45c5a49bf03e9ca2659ef3981f935c43c3dce2
yskim621 <yskim62100@gmail.com>
2022-06-18 00:09:55 +0900

    Updating BoardService

--- src/main/java/game/guidebook/service/BoardService.java
+++ src/main/java/game/guidebook/service/BoardService.java
@@ -2,6 +2,7 @@ package game.guidebook.service;
 
 import game.common.persist.query.SearchResult;
 import game.guidebook.api.dto.BoardDto;
+import game.guidebook.api.dto.BoardForm;
 import game.guidebook.domain.Board;
 import game.guidebook.service.dto.QueryParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -12,13 +13,13 @@ import java.util.List;
 public interface BoardService {
 
     // 전체 보기를 위한 메소드
-    public SearchResult<BoardDto> findAll(QueryParam query_param, int offset, int limit);
+    public SearchResult<BoardDto> findAll(QueryParam query_param);
 
     // 상세 보기를 위한 메소드
     public Board detail(Long id);
 
     // 데이터 생성을 위한 메소드
-    public void create(MultipartHttpServletRequest request);
+    public void create(BoardForm boardForm);
 
     // 데이터 수정을 위한 메소드
     public Long update(Long id, Board board);
