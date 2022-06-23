1aa5264da8cb127ea9182fd80416c2b27844af9f
yskim621 <yskim62100@gmail.com>
2022-06-23 10:28:54 +0900

    Updating boardDetail Front

--- src/main/resources/static/js/board/detail.js
+++ src/main/resources/static/js/board/detail.js
@@ -4,27 +4,25 @@ gtag('js', new Date());
 
 gtag('config', 'G-2H59G05B23');
 
-$("#btn_list").click(function () {
-    window.location.href = Helper.basePath + "/board/list"
-});
+// $("#btn_list").click(function () {
+//     window.location.href = Helper.basePath + "/board/list"
+// });
 
 var id = $("#id").val();
-$("#btn_update").click(function () {
-    window.location.href = Helper.basePath + "/board/update/" + id
-});
+// $("#btn_update").click(function () {
+//     window.location.href = Helper.basePath + "/board/update/" + id
+// });
 
 $("#btn_delete").click(function () {
     Helper.message.confirm('정말로 삭제하겠습니까？', function(index)
     {
-        Helper.post(Helper.basePath + '/board/delete/' + id, function(data) {
+        Helper.post(Helper.basePath + '/api/board/delete/' + id, function(data) {
             if(data.success)
             {
                 $("#bootTable").bootstrapTable("removeByUniqueId", id);
                 $("#bootTable").bootstrapTable("refresh");
-                $("#detailTable tr").bootstrapTable("removeByUniqueId", id);
-                $("#detailTable tr").bootstrapTable("refresh");
-                $("#assyNo").val("");
                 Helper.message.suc('삭제됨!');
+                window.location.href = Helper.basePath + '/board/list';
             }else
             {
                 Helper.message.err('삭제 실패：' + data.message);
