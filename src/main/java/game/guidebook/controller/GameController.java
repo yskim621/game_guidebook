c6814cd2bb46cb3234c0bb6f574f1fc4e6816990
yskim621 <yskim62100@gmail.com>
2022-06-27 10:36:34 +0900

    Updating GameController

--- src/main/java/game/guidebook/controller/GameController.java
+++ src/main/java/game/guidebook/controller/GameController.java
@@ -2,6 +2,7 @@ package game.guidebook.controller;
 
 import game.guidebook.service.GameService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
