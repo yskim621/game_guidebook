406cd837a9c5cdacceb3eead3d9430eebcca7e1b
yskim621 <yskim62100@gmail.com>
2022-06-29 00:18:59 +0900

    Updating BoardJpaRepository

--- src/main/java/game/guidebook/repository/BoardJpaRepository.java
+++ src/main/java/game/guidebook/repository/BoardJpaRepository.java
@@ -1,6 +1,8 @@
 package game.guidebook.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import game.guidebook.domain.Board;
+import game.guidebook.domain.QBoard;
 import game.guidebook.service.dto.QueryParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,21 +12,33 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import java.util.List;
 
+import static game.guidebook.domain.QBoard.*;
+
 @Slf4j
 @Repository
 @RequiredArgsConstructor
 public class BoardJpaRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
 
     public List<Board> findAll(QueryParam query_param){
         int size = query_param.getPageSize();
         int start = (query_param.getPageNumber() - 1) * size;
 
-        return em.createQuery("select b from Board b", Board.class)
-                .setFirstResult(start)
-                .setMaxResults(size)
-                .getResultList();
+//        return em.createQuery("select b from Board b", Board.class)
+//                .setFirstResult(start)
+//                .setMaxResults(size)
+//                .getResultList();
+
+        // QueryDSL 적용
+        return queryFactory
+                .selectFrom(board)
+                .orderBy(board.id.desc())
+                .offset(start)
+                .limit(size)
+                .fetch();
     }
 
     public Board findOne(Long id) {
