f65f26e7fdbb512825e6861b19a5d22f4fbe3b44
yskim621 <yskim62100@gmail.com>
2022-07-03 02:42:23 +0900

    Updating RegisterController

--- src/main/java/game/guidebook/controller/ReplyController.java
+++ src/main/java/game/guidebook/controller/ReplyController.java
@@ -1,7 +1,9 @@
 package game.guidebook.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 public class ReplyController extends BaseController {
 }
