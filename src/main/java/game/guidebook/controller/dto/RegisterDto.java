f02755aab30f2df573d55f5b45e8a0df198344a0
yskim621 <yskim62100@gmail.com>
2022-07-10 00:33:46 +0900

    Updating register front end

--- src/main/java/game/guidebook/controller/dto/RegisterDto.java
+++ src/main/java/game/guidebook/controller/dto/RegisterDto.java
@@ -13,8 +13,9 @@ public class RegisterDto implements Serializable
 	private static final long serialVersionUID = 1L;
 
 	private String name;
+	private String nickname;
 	private String mobile;
-	private String email;
+//	private String email;
 	private String password;
 	private String ip;
 	private String validCode;
