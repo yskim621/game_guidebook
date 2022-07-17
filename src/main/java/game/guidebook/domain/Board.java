0d0361052c8830feb4a311b6860a64570d208469
yskim621 <yskim62100@gmail.com>
2022-07-18 00:57:46 +0900

    Updating Board

--- src/main/java/game/guidebook/domain/Board.java
+++ src/main/java/game/guidebook/domain/Board.java
@@ -3,9 +3,7 @@ package game.guidebook.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -31,4 +29,7 @@ public class Board {
 
 //    private Long userId;
 
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
 }
