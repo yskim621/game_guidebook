2a2ca6c60136f8708884ee830a605af34bc1a35e
yskim621 <yskim62100@gmail.com>
2022-07-19 00:12:13 +0900

    Updating GuidebookApplication class

--- src/main/java/game/guidebook/GuidebookApplication.java
+++ src/main/java/game/guidebook/GuidebookApplication.java
@@ -1,9 +1,15 @@
 package game.guidebook;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.persistence.EntityManager;
+
+@EnableJpaAuditing
 @SpringBootApplication(exclude  = { SecurityAutoConfiguration.class })
 public class GuidebookApplication {
 
@@ -11,4 +17,8 @@ public class GuidebookApplication {
 		SpringApplication.run(GuidebookApplication.class, args);
 	}
 
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
 }
