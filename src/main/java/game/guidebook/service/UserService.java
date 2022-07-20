3bc4cc1a343c8c239b61811997c08d22bc8eaa98
yskim621 <yskim62100@gmail.com>
2022-07-21 00:07:01 +0900

    Updating UserService

--- src/main/java/game/guidebook/service/UserService.java
+++ src/main/java/game/guidebook/service/UserService.java
@@ -5,6 +5,8 @@ import game.guidebook.domain.User;
 import net.nurigo.java_sdk.api.Message;
 import org.json.simple.JSONObject;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -21,7 +23,7 @@ public interface UserService {
 
     public Long register(RegisterDto dto);
 
-    public Map<String, Object> login(String name, String password);
+    public Map<String, Object> login(String name, String password, HttpServletRequest request, HttpServletResponse response);
 
     public String sendSms(String mobileNo);
 }
