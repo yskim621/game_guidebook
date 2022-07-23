d4659dd63e1ac7c5ee3ceeebb2a07189924432f9
yskim621 <yskim62100@gmail.com>
2022-07-24 00:22:37 +0900

    Updating UserController

--- src/main/java/game/guidebook/controller/UserController.java
+++ src/main/java/game/guidebook/controller/UserController.java
@@ -1,19 +1,17 @@
 package game.guidebook.controller;
 
-import game.guidebook.common.utils.UserUtils;
-import game.guidebook.constants.SysConstants;
 import game.guidebook.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("user")
@@ -47,4 +45,14 @@ public class UserController extends BaseController {
 //        }
         return "user/login";
     }
+
+    @GetMapping("logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "user/login";
+    }
+
 }
