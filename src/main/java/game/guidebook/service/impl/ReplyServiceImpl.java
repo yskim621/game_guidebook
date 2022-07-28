bc807ec29b8baad2ab860bd6299a3f8776d53503
yskim621 <yskim62100@gmail.com>
2022-07-29 01:57:35 +0900

    Updating ReplyServiceImpl

--- src/main/java/game/guidebook/service/impl/ReplyServiceImpl.java
+++ src/main/java/game/guidebook/service/impl/ReplyServiceImpl.java
@@ -1,8 +1,10 @@
 package game.guidebook.service.impl;
 
 import game.guidebook.service.ReplyService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ReplyServiceImpl implements ReplyService {
 
