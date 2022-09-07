eb4542e273a2c8457e72c51b47fe5d44d3ee3cee
yskim621 <yskim62100@gmail.com>
2022-09-08 00:24:42 +0900

    Adding CommonService.java

---
+++ src/main/java/game/guidebook/service/CommonService.java
@@ -0,0 +1,23 @@
+package game.guidebook.service;
+
+import game.guidebook.common.BaseBasicTableEntity;
+import org.hibernate.type.BasicType;
+
+import java.util.List;
+
+public interface CommonService
+{
+//	public boolean backupAllData(String companyId);
+//
+//	public boolean delete(BasicType basicType, Long id);
+//
+//	public List<BaseBasicTableEntity> getBasicInfoList(BasicType type);
+//
+//	public List<BaseBasicTableEntity> getCommBasicInfoList(BasicType type);
+//
+//	public List<BaseBasicTableEntity> getCommBasicInfoList(BasicType type, String companyId);
+//
+//	public int getNextSort(BasicType type);
+//
+//	public String getNextCode(BasicType type);
+}
